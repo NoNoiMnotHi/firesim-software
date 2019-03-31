d5e9baf8d4f8cb6693a4fd84f7b09329818a7a95
Farzad Farshchi <farzadfr@gmail.com>
2019-03-31 22:52:11 +0000

    create a firemarshal workload for darkent-nvdla and modify the download script accordingly.

--- get-darknet.sh
+++ get-darknet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+mkdir -p workloads/darknet-nvdla/overlay/root
+cd workloads/darknet-nvdla
 wget -O darknet-nvdla.tar.gz http://www.ittc.ku.edu/~farshchi/darknet-nvdla.tar.gz
-tar xzf darknet-nvdla.tar.gz -C br/buildroot-overlay/usr/
+tar xzf darknet-nvdla.tar.gz -C overlay/root
